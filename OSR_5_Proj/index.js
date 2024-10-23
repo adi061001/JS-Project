2823e25a447b2f6aca1fca94923bcd93e1fa16d5
adi061001 <adityasharma@thoughtwin.com>
2024-10-23 19:21:50 +0530

    the Login and Registration form Add _V4

--- OSR_5_Proj/index.js
+++ OSR_5_Proj/index.js
@@ -1,10 +1,4 @@
-console.log(window.screenX)
-
-function headerScroll() {
-  if (window.scrollY > 0) {
-    console.log("change it")
-  }
-}
+// local storage Contact email address
 
 let contactEmailIds = []
 function contacatEmailId(event) {
@@ -18,6 +12,20 @@ function contacatEmailId(event) {
   let m = localStorage.getItem("email")
   console.log([m])
 }
+
+// nav bar color changer
+function scrollNavbar() {
+  let scrollNum = window.scrollY
+
+  if (scrollNum > 360) {
+    document.getElementById("navbarscroll").style.background = "#413551f2"
+  } else if (scrollNum < 360) {
+    document.getElementById("navbarscroll").style.background = ""
+  }
+}
+window.addEventListener("scroll", scrollNavbar)
+
+//card slider
 
 function slideChange_First() {
   document.getElementById("div_id_sec-2").style.display = "block"
@@ -43,4 +51,19 @@ function slideChange_Second() {
   document.getElementById("last-div-slider-button_second").style.border = "none"
   document.getElementById("last-div-slider-button-first").style.border =
     "1px solid black"
+}
+
+//login page open
+
+function loginPageOpen() {
+  // alert("go")
+  document.getElementById("closeLOginForm").style.display = "block"
+  document.getElementById("Loginform").style.display = "block"
+  document.getElementById("maindbody").style.overflow = "hidden"
+}
+
+//Close login form
+function closeForm() {
+  document.getElementById("closeLOginForm").style.display = "none"
+  document.getElementById("Loginform").style.display = "none"
 }
