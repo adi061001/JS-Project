328704fc54dba126b13514cd8fc96d0bd1423cf6
adi061001 <adityasharma@thoughtwin.com>
2024-10-25 18:50:27 +0530

    the login registration done on localstorage V_6

--- OSR_5_Proj/index.js
+++ OSR_5_Proj/index.js
@@ -67,3 +67,102 @@ function closeForm() {
   document.getElementById("closeLOginForm").style.display = "none"
   document.getElementById("Loginform").style.display = "none"
 }
+
+//registarion open
+function registrationPageOpen() {
+  document.getElementById("Regiestrationform").style.display = "block"
+  document.getElementById("maindbody").style.overflow = "hidden"
+}
+
+// registration save in local
+let localStorageaddRegistration =
+  JSON.parse(localStorage.getItem("registrationData")) || []
+function registartionLocaclSave(event) {
+  event.preventDefault()
+  // alert("hello")
+  let regPass = document.getElementById("input_Password-registartion").value
+  let regrePass = document.getElementById(
+    "input_re-passwordcheck-registartion"
+  ).value
+
+  if (regPass == regrePass) {
+    let registarPeople = {
+      Name: document.getElementById("input_fillname-registartion").value,
+      Email: document.getElementById("input_Emailid-registartion").value,
+      passwrd: document.getElementById("input_Password-registartion").value,
+    }
+
+    localStorageaddRegistration.push(registarPeople)
+
+    localStorage.setItem(
+      "registrationData",
+      JSON.stringify(localStorageaddRegistration)
+    )
+
+    document.getElementById("input_fillname-registartion").value = ""
+    document.getElementById("input_Emailid-registartion").value = ""
+    document.getElementById("input_Password-registartion").value = ""
+    document.getElementById("input_re-passwordcheck-registartion").value = ""
+    // window.location.replace(
+    //   "./allMethodSecondindexpages/StringPage/String.html"
+    // )
+    window.location.reload()
+  } else {
+    document.getElementById("error_msg_1").style.display = "block"
+    // document.getElementById("error_msg").innerText =
+    //   "The Password is not Match!"
+  }
+}
+let registrationDataUser = []
+let localDataUser = localStorage.getItem("registrationData")
+registrationDataUser = JSON.parse(localDataUser)
+
+function checkEmail() {
+  let checkEmail = document.getElementById("input_Emailid-registartion").value
+  let status = undefined
+  console.log(localDataUser)
+  console.log(localDataUser != null)
+  if (localDataUser != null) {
+    status = registrationDataUser.some(
+      (emailcheck) => emailcheck.Email == checkEmail
+    )
+    console.log(status)
+  }
+
+  if (status) {
+    let btn = document.getElementById("btn_1_registration")
+    btn.setAttribute("disabled", true)
+    document.getElementById("error_msg").style.display = "block"
+  }
+}
+
+function checkemail_1() {
+  document.getElementById("error_msg").style.display = "none"
+  let btn = document.getElementById("btn_1_registration")
+  btn.removeAttribute("disabled")
+}
+
+function checkpassword() {
+  document.getElementById("error_msg_1").style.display = "none"
+}
+
+//Login Form
+function loginSuccsec(event) {
+  event.preventDefault()
+  let loginEmail = document.getElementById("emailIdLogin").value
+  let loginpassword = document.getElementById("PasswordLogin").value
+  let local = []
+  let localget = localStorage.getItem("registrationData")
+
+  if (localget != null) {
+    local = JSON.parse(localget)
+    let mailidcheck = local.some((email) => email.Email == loginEmail)
+    console.log(mailidcheck)
+    let passwordcheck = local.some((email) => email.passwrd == loginpassword)
+    console.log(passwordcheck)
+
+    if (mailidcheck && passwordcheck) {
+      document.getElementById("login_Msg").style.display = "block"
+    }
+  }
+}
